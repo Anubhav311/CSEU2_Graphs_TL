b2e3c93af1fbd0bf72813195e769c811447965b3
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-06 10:19:06 +0530

    created Graph class in ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -14,6 +14,23 @@ class Queue:
     def size(self):
         return len(self.storage)
 
+class Graph:
+    """
+    Represent a graph as a dictionary
+    of vertexes mapping labels to edge.
+    """
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise KeyError("That vertex does not exist!")
 
 def earliest_ancestor(ancestors, starting_node):
     pass
