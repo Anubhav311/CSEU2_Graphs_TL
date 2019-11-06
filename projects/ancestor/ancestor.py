a498ddb78d8e8927a129b7c81c20494bfdf46983
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-06 12:05:58 +0530

    completed earliest_ancestor function

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -6,7 +6,7 @@ class Queue:
         self.storage.append(value)
 
     def dequeue(self):
-        if self.storage > 0:
+        if self.size() > 0:
             return self.storage.pop(0)
         else:
             return None
@@ -33,4 +33,47 @@ class Graph:
             raise KeyError("That vertex does not exist!")
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # 1. BUILD THE GRAPH
+    # instantiate a new graph object
+    graph = Graph()
+    # loop over all pairs in ancestors
+    for pair in ancestors:
+        # add pair[0] and pair[1] to the graph
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # build the edges in reverse
+        graph.add_edge(pair[1], pair[0])
+
+
+    # 2. DO A BFS (with paths)
+    # create a queue
+    qq = Queue()
+    # enqueue starting node inside a list
+    qq.enqueue([starting_node])
+    # set a max path length to 1
+    max_path_length = 1
+    # set initial earliest ancestor
+    earliest_ancestor = -1
+    # while queue has contents
+    while qq.size() > 0:
+        # dequeue the path
+        path = qq.dequeue()
+        # get the last vert
+        vert = path[-1]
+        # if path is longer or equal to max path length and the value is less than earliest ancestor, 
+        # or if the path is longer than max path length
+        if (len(path) >= max_path_length and vert < earliest_ancestor) or (len(path) > max_path_length):
+            # set the earliest ancestor to the vert
+            earliest_ancestor = vert
+            # set the max path length to the length of the path
+            max_path_length = len(path)
+        # loop over each neighbor in the graph's vertices at index of that
+        for neighbor in graph.vertices[vert]:
+            # make a copy of the path 
+            path_copy = list(path)
+            # append neighbor to the copied path
+            path_copy.append(neighbor)
+            # then enqueue the copied path
+            qq.enqueue(path_copy)
+    # return earliest ancestor
+    return earliest_ancestor
